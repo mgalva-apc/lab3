5e458f13567fd7e9d8fc7c3aa31505a7e9f16959
Bea-Alva <116138375+Bea-Alva@users.noreply.github.com>
2023-02-16 13:16:55 +0800

    yas

---
+++ ci4/app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>CodeIgniter Tutorial</title>
+</head>
+    <body>
+        <h2> This page is all about orange juice</h2>
+</body>
+</html> 
---
+++ ci4/app/Views/pages/home.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>CodeIgniter Tutorial</title>
+</head>
+    <body>
+        <h2> Ad Astra Abyssosque, Travelers!</h2>
+</body>
+</html> 
